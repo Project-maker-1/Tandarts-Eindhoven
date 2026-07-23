868701e3cb65ecfa0178b6d926a72bb7be5d25c7
Husam <523912.ali@gmail.com>
2026-07-23 17:22:23 +0200

    validatie van email, naam en telefoon toegevoegd.

--- validatie.js
+++ validatie.js
@@ -1,14 +1,28 @@
 const formulier = document.querySelector('#formulier')
-const naam = document.querySelector('#naam')
-const naamfout = document.querySelector('#naam-fout')
+
+const inputs = {
+    naam: document.querySelector('#naam'),
+    email: document.querySelector('#email'),
+    telefoon: document.querySelector('#telefoon'),
+}
+
+const fouten = {
+    naam: document.querySelector('#naam-fout'),
+    email: document.querySelector('#email-fout'),
+    telefoon: document.querySelector('#telefoon-fout'),
+}
 
 formulier.addEventListener('submit', function(event) {
     
-    if (naam.value === "") {
-        event.preventDefault()
-        naamfout.classList.remove('hidden')
-    } else {
-        name = true
-        naamfout.classList.add('hidden')
-    }
+    Object.keys(inputs).forEach(function(sleutel) {
+        if (inputs[sleutel].value === "") {
+            event.preventDefault()
+            fouten[sleutel].classList.remove("hidden")
+        }
+        
+        else {
+    fouten[sleutel].classList.add('hidden')
+}
+    })
 })
+
